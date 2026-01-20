20b70ecfdaec687e22d449b7334ae9f07ae47288
Oshied <144242849+de-ishi@users.noreply.github.com>
2026-01-20 17:03:52 +0200

    Update restLoader.lua

--- restLoader.lua
+++ restLoader.lua
@@ -1,1 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/de-ishi/rest/refs/heads/main/BreakALuckyBlock.lua"))()
+loadstring(game:HttpGet("https://github.com/de-ishi/rest/blob/main/EscapeTsunamiForBrainrots.lua"))()
+
