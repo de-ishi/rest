43fcb6f3c4d62e61d72740916366a812777827fc
Oshied <144242849+de-ishi@users.noreply.github.com>
2026-02-22 19:57:36 +0200

    Update restLoader.lua

--- restLoader.lua
+++ restLoader.lua
@@ -1,3 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/de-ishi/rest/refs/heads/main/Garden_Horizons.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/de-ishi/rest/refs/heads/main/BreakALuckyBlock.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/de-ishi/rest/refs/heads/main/EscapeTsunamiForBrainrots.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/de-ishi/rest/refs/heads/main/TapSimulator.lua"))()
